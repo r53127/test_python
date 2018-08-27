55770483f167e54a0fda76dc409e6e09f363cf9d
r53127 <rnkn@163.com>
2018-08-27 15:02:58 +0800

    update copyfile

--- copyfile.py
+++ copyfile.py
@@ -1,13 +1,17 @@
 import re
-
 import os
-
+import shutil
 
 for root, dirs, files in os.walk("E:\俄罗斯方块视频"):
-    # for name in files:
-    #     print(name)
     for dir in dirs:
-        print(os.path.join(root, dir))
-        dircname = re.search("[\u5408][\u5e76]+$", dir)
-        if dircname:
-            f
+        # print(os.path.join(root, dir))
+        dirname = re.search("[\u5408\u5e76]+$", dir)
+        if dirname:
+            listfile=os.listdir(root+'\\'+dir)
+            source_path = os.path.join(root+'\\'+dir, listfile[0])
+            print('源文件：',source_path)
+            newfilename='e:\\test\\'+listfile[0]
+            print('目标文件:',newfilename)
+            shutil.copyfile(source_path,newfilename)
+
+
